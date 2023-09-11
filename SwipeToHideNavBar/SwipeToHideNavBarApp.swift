020eade363a46ca48a5bf0ac5ce9bceba645c9d6
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-11 15:29:09 +0300

    Initial Commit

---
+++ SwipeToHideNavBar/SwipeToHideNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeToHideNavBarApp.swift
+//  SwipeToHideNavBar
+//
+//  Created by Yunus Emre Taşdemir on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwipeToHideNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
